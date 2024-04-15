5cb09e65f158f568857d818ab301ded4f27833d8
Josh Taylor <taylor.joshua88@gmail.com>
2024-04-15 12:33:10 -0500

    Upgrade packages

--- modules/user/programs/nvim/colorschemes.nix
+++ modules/user/programs/nvim/colorschemes.nix
@@ -3,22 +3,24 @@
 {
   programs.nixvim.colorschemes.catppuccin = {
     enable = true;
-    flavour = "mocha";
-    terminalColors = true;
+    settings = {
+      flavour = "mocha";
+      term_colors = true;
 
-    integrations = {
-      aerial = true;
-      cmp = true;
-      dap.enabled = true;
-      fidget = true;
-      gitsigns = true;
-      indent_blankline.enabled = true;
-      mason = true;
-      mini.enabled = true;
-      telescope.enabled = true;
-      treesitter = true;
-      treesitter_context = true;
-      which_key = true;
+      integrations = {
+        aerial = true;
+        cmp = true;
+        dap.enabled = true;
+        fidget = true;
+        gitsigns = true;
+        indent_blankline.enabled = true;
+        mason = true;
+        mini.enabled = true;
+        telescope.enabled = true;
+        treesitter = true;
+        treesitter_context = true;
+        which_key = true;
+      };
     };
   };
 }
